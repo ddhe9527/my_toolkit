c84fc19ca2e4415f9dab8679fb069cfde75090f5
ddhe9527 <heduoduo321@163.com>
2020-09-26 15:09:55 +0800

    Update mycnf_helper.sh
    增加对依赖包的yum安装

--- mycnf_helper.sh
+++ mycnf_helper.sh
@@ -823,9 +823,9 @@ fi
 echo "Installing necessary packages..."
 if [ $OS_VER_NUM -lt 8 ]
 then
-    yum install -y net-tools libaio libaio-devel numactl-libs numactl autoconf ntp xz perl-Module* &>/dev/null
+    yum install -y net-tools libaio libaio-devel numactl-libs ncurses-compat-libs numactl autoconf xz perl-Module* ntp &>/dev/null
 else
-    yum install -y net-tools libaio libaio-devel numactl-libs numactl autoconf xz perl-Module* &>/dev/null
+    yum install -y net-tools libaio libaio-devel numactl-libs ncurses-compat-libs numactl autoconf xz perl-Module* &>/dev/null
 fi
 if [ $? -eq 1 ]
 then
